19bed984a511f1cee6445d78cc1cf90f9abb75d1
Joakim Teixeira <setokim1@hotmail.com>
2020-10-28 11:21:27 -0300

    Add photo fetch handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 const App = () => {
+	const [photos, setPhotos] = useState([])
+
+	const fetchPhotos = async () => {
+		const response = await axios.get('https://api.pexels.com/v1/search', {
+			params: {
+				query: 'cats',
+			},
+			headers: {
+				Authorization: process.env.REACT_APP_PEXELS_API_KEY,
+			},
+		})
+		setPhotos(response.data.photos)
+	}
+
+	useEffect(() => {
+		fetchPhotos()
+	}, [])
+
 	return <div>Image search app</div>
 }
 
